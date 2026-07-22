33251ab415bec88f0acac0d67f0129a18e157d28
Abdoul Razack ZONGO <tegawindeabdoul@gmail.com>
2026-07-22 15:26:14 -0400

    Ajoute la gestion de plusieurs capteurs via vector

--- main.cpp
+++ main.cpp
@@ -8,19 +8,6 @@ using namespace std;
 
 int main()
 {
-//     Capteur monCapteur;
-
-// monCapteur.setTemperature(37);
-// monCapteur.afficherInfo();
-    
-
-// monCapteur.setTemperature(100);
-// monCapteur.afficherInfo();
-
-
-// monCapteur.setTemperature(-50);
-// monCapteur.afficherInfo();
-
 
 vector<Capteur> capteurs;
 capteurs.push_back(Capteur("celsius"));
